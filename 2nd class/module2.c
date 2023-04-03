ce6ac875a10b0f2533ae3cede37ef7db68c419ca
Renajaj Shammia <renajajshammia2002@gmail.com>
2023-04-03 19:24:12 +0600

    type code

---
+++ 2nd class/module2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=100)
+    {
+        printf("I will eat burger");
+    }
+
+    else if(tk>=50)
+    {
+        printf("I will eat fucka");
+    }
+    else
+    {
+        printf("Iwill not eat anything today");
+    }
+    return 0;
+}
